117a90831f580728fa0416b62091c590ba698bd8
anjalinaragude <96512511+anjalinaragude@users.noreply.github.com>
2026-03-14 21:54:15 +0530

    Update BankAppApplication.java

--- src/main/java/com/example/bankapp/BankAppApplication.java
+++ src/main/java/com/example/bankapp/BankAppApplication.java
@@ -2,12 +2,11 @@ package com.example.bankapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class BankAppApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BankAppApplication.class, args);
     }
-
 }
